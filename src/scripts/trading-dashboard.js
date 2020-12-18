15b0160f729b358b32407eadc7ead4091e6b93a8
Satyam Gaur <sgsatyamg@gmail.com>
2020-12-18 18:45:10 +0530

    MFI Done

--- src/scripts/trading-dashboard.js
+++ src/scripts/trading-dashboard.js
@@ -1684,5 +1684,60 @@ createCMFEvent.addEventListener('click', function () {
 })
 
 
+// Creating a MFI Event
+let createMFIEvent = document.getElementById('create-mfi-event');
+createMFIEvent.addEventListener('click', function () {
+  console.log('clicked');
+  // Showing the creating event spinner
+  createEvent.style.display = 'none';
+  creatingEventLoader.style.display = 'block';
+
+  // Initialising the event
+  let eventName = newEventInitialiser(2);
+
+  // Creating a bubble in the show all events modal
+  let modalBubblesContainer = document.getElementById('modal-bubbles-container');
+  let a = createNewBubbleForModal(eventName);
+  modalBubblesContainer.append(a);
+
+  // Creating new Monitor Div
+  let monitorDiv = createNewEventMonitorDiv(eventName);
+  document.getElementById('outer-screen-div').append(monitorDiv);
+
+  // Customizing the URL
+  let MFICross = document.getElementById('mfi-cross');
+  let MFIUpperThreshold = document.getElementById('mfi-upper-threshold');
+  let MFILowerThreshold = document.getElementById('mfi-lower-threshold');
+
+  var xmlHttp = new XMLHttpRequest();
+  let url = backendBaseURL + 'Dashboard/mfi?';
+  url += ('upperThreshold=' + MFIUpperThreshold.value.toString());
+  url += ('&lowerThreshold=' + MFILowerThreshold.value.toString());
+  url += ('&cross=' + (MFICross.checked ? 1 : 0));
+  console.log(url);
+
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      creatingEventLoader.style.display = 'none';
+      monitorEvent.style.display = 'block';
+
+      let fetchedData = JSON.parse(xmlHttp.responseText);
+      addFetchedData(eventName, fetchedData);
+
+      console.log('Final click');
+      // Opening the first Screen tab of the created monitor screens
+      changeCurrentEvent(eventName);
+      document.getElementById(eventName + '-screen1-tab').childNodes[0].click();
+      document.getElementById(eventName + '-screen1-title').innerText = 'MFI Oversold';
+      document.getElementById(eventName + '-screen2-title').innerText = 'MFI Overbought';
+    }
+  }
+  console.log('getting');
+  xmlHttp.open("GET", url, true); // true for asynchronous 
+  xmlHttp.setRequestHeader('Authorization', 'Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o');
+  xmlHttp.send(null);
+})
+
+
 // Things to do
 // 1) Pass on personalized jwt token.
